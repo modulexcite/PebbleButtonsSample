0a64621176f826d8d2920a41a1d0c317dc688077
elbruno <elbruno@users.noreply.github.com>
2014-05-15 09:18:53 -0700

    buttons move image
    click buttons move image

--- src/main.c
+++ src/main.c
@@ -1,22 +1,62 @@
 #include <pebble.h>
 
 Window *window;
-TextLayer *text_layer;
 GBitmap *nino_bitmap;
 BitmapLayer *nino_layer;
+int imageYLocation;
+int difY;
 
+void on_animation_stopped(Animation *anim, bool finished, void *context)
+{
+    //Free the memoery used by the Animation
+    property_animation_destroy((PropertyAnimation*) anim);
+}
+ 
+void animate_layer(Layer *layer, GRect *start, GRect *finish, int duration, int delay)
+{
+    //Declare animation
+    PropertyAnimation *anim = property_animation_create_layer_frame(layer, start, finish);
+ 
+    //Set characteristics
+    animation_set_duration((Animation*) anim, duration);
+    animation_set_delay((Animation*) anim, delay);
+ 
+    //Set stopped handler to free memory
+    AnimationHandlers handlers = {
+        //The reference to the stopped handler is the only one in the array
+        .stopped = (AnimationStoppedHandler) on_animation_stopped
+    };
+    animation_set_handlers((Animation*) anim, handlers, NULL);
+ 
+    //Start animation!
+    animation_schedule((Animation*) anim);
+}
 
 /* Buttons Handlers */
 void up_click_handler(ClickRecognizerRef recognizer, void *context)
 {
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Button Up !"); 
-  vibes_short_pulse();
+  difY = imageYLocation - 10;
+  if(difY >= 0)
+  {
+    GRect start = GRect(60, imageYLocation, 48, 48);
+    GRect finish = GRect(60, difY, 48, 48);
+    animate_layer(bitmap_layer_get_layer(nino_layer), &start, &finish, 300, 500);
+    imageYLocation = difY;
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Button Up ! Y: " + imageYLocation); 
+  }
 }
  
 void down_click_handler(ClickRecognizerRef recognizer, void *context)
 {
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Button Down !"); 
-  vibes_long_pulse();
+  int difY = imageYLocation + 10;
+  if(difY <= 120)
+  {
+    GRect start = GRect(60, imageYLocation, 48, 48);
+    GRect finish = GRect(60, difY, 48, 48);
+    animate_layer(bitmap_layer_get_layer(nino_layer), &start, &finish, 300, 500);
+    imageYLocation = difY;
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Button Down ! Y: " + imageYLocation); 
+  }
 }
  
 void select_click_handler(ClickRecognizerRef recognizer, void *context)
@@ -30,47 +70,32 @@ void click_config_provider(void *context)
     window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
     window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
 }
+
 void handle_init(void) {
 	// Create a window and text layer
 	window = window_create();
-	text_layer = text_layer_create(GRect(60, 62, 144, 14));
 	
-	// Set the text, font, and text alignment
-	text_layer_set_text(text_layer, "Messi");
-	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
-	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-	
-	// Add the text layer to the window
-	//layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
-
   //Load bitmaps into GBitmap structures
   //The ID you chose when uploading is prefixed with 'RESOURCE_ID_'
   nino_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FOOTBALL);
    
   //Create BitmapLayers to show GBitmaps and add to Window
-  //Sample images are 144 x 50 pixels
-  nino_layer = bitmap_layer_create(GRect(98, 60, 48, 48));
+  nino_layer = bitmap_layer_create(GRect(60, 60, 48, 48));
   bitmap_layer_set_bitmap(nino_layer, nino_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(nino_layer));
-     
+  imageYLocation = 60;
+  
   // buttons handlers
   window_set_click_config_provider(window, click_config_provider);
   
 	// Push the window
 	window_stack_push(window, true);
-	
-	// App Logging!
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
 }
 
 void handle_deinit(void) {
-	// Destroy the text layer
-	text_layer_destroy(text_layer);
 	
-  //Destroy GBitmaps
+  //Destroy GBitmaps and BitmapLayers
   gbitmap_destroy(nino_bitmap);
-   
-  //Destroy BitmapLayers
   bitmap_layer_destroy(nino_layer);
     
 	// Destroy the window
