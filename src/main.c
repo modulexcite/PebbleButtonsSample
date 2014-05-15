64b37b31326f399bc563d30a674b527c2a61a044
elbruno <elbruno@users.noreply.github.com>
2014-05-15 08:56:24 -0700

    Add image
    change layout and add image to main app

--- src/main.c
+++ src/main.c
@@ -2,22 +2,27 @@
 
 Window *window;
 TextLayer *text_layer;
+GBitmap *nino_bitmap;
+BitmapLayer *nino_layer;
 
 
 /* Buttons Handlers */
 void up_click_handler(ClickRecognizerRef recognizer, void *context)
 {
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Button Up !"); 
+  vibes_short_pulse();
 }
  
 void down_click_handler(ClickRecognizerRef recognizer, void *context)
 {
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Button Down !"); 
+  vibes_long_pulse();
 }
  
 void select_click_handler(ClickRecognizerRef recognizer, void *context)
 {
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Button Select !"); 
+  vibes_double_pulse();
 }
 void click_config_provider(void *context)
 {
@@ -28,16 +33,26 @@ void click_config_provider(void *context)
 void handle_init(void) {
 	// Create a window and text layer
 	window = window_create();
-	text_layer = text_layer_create(GRect(0, 0, 144, 154));
+	text_layer = text_layer_create(GRect(60, 62, 144, 14));
 	
 	// Set the text, font, and text alignment
-	text_layer_set_text(text_layer, "El Bruno, buttons sample!");
-	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+	text_layer_set_text(text_layer, "Messi");
+	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
 	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
 	
 	// Add the text layer to the window
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+	//layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
 
+  //Load bitmaps into GBitmap structures
+  //The ID you chose when uploading is prefixed with 'RESOURCE_ID_'
+  nino_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FOOTBALL);
+   
+  //Create BitmapLayers to show GBitmaps and add to Window
+  //Sample images are 144 x 50 pixels
+  nino_layer = bitmap_layer_create(GRect(98, 60, 48, 48));
+  bitmap_layer_set_bitmap(nino_layer, nino_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(nino_layer));
+     
   // buttons handlers
   window_set_click_config_provider(window, click_config_provider);
   
@@ -52,6 +67,12 @@ void handle_deinit(void) {
 	// Destroy the text layer
 	text_layer_destroy(text_layer);
 	
+  //Destroy GBitmaps
+  gbitmap_destroy(nino_bitmap);
+   
+  //Destroy BitmapLayers
+  bitmap_layer_destroy(nino_layer);
+    
 	// Destroy the window
 	window_destroy(window);
 }
